b9a4d6786968b3eb85d6bd72d0621959c2e42246
Ananthan <ananthan@hotmail.ca>
2021-06-01 16:10:36 -0400

    created partial content page

--- .history/routes/index_20210601155705.ts
+++ .history/routes/index_20210601155705.ts
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next)
 /* GET home page. */
 router.get('/home', function(req, res, next) 
 {
-  res.render('index', { title: 'Home', page: 'home'  });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 
 /* GET about page. */
--- .history/routes/index_20210601155858.ts
+++ .history/routes/index_20210601155858.ts
@@ -5,13 +5,13 @@ export default router;
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
-  res.render('index', { title: 'Home', page: 'home' });
+  res.render('index', { title: 'Home'});
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) 
 {
-  res.render('index', { title: 'Home', page: 'home'  });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 
 /* GET about page. */
